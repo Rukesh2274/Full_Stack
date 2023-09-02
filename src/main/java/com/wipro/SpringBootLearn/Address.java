92f6cce8ad50a51d8670918bbbe66720443e45a1
Rukesh2274 <20113119@student.hindustanuniv.ac.in>
2023-09-02 10:59:53 +0530

    update

---
+++ src/main/java/com/wipro/SpringBootLearn/Address.java
@@ -0,0 +1,25 @@
+package com.wipro.SpringBootLearn;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Address {
+
+	private String city;
+    private String state;
+
+    public Address() { System.out.println("Address Constructor");}
+    public Address(String c, String s)
+    {
+        this.city = c;
+        this.state = s;
+    }
+    public void setCity(String c) { this.city = c; }
+    public void setState(String s) {this.state = s ; }
+    public String getCity() { return this.city ;}
+    public String getState() { return this.state ; }
+    public void Display()
+    {
+        System.out.println("City:" + this.city + " State:" + this.state);
+    }
+}
--- src/main/java/com/wipro/SpringBootLearn/Student.java
+++ src/main/java/com/wipro/SpringBootLearn/Student.java
@@ -1,8 +1,10 @@
 package com.wipro.SpringBootLearn;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class Student {
 	
     private int rno;
